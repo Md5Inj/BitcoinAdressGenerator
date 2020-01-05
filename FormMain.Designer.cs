7f0a3badb7c88fab3c48c3bd6d9e7aa18aba31c2
Maxim Romashko <super.romaha@gmail.com>
2020-01-05 20:11:46 +0300

    first commit

---
+++ FormMain.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace RandomText
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerThread = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonGenerateRandom = new System.Windows.Forms.Button();
+            this.textBoxDelimiter = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBarUpdateBase = new System.Windows.Forms.ProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerThread
+            // 
+            this.timerThread.Tick += new System.EventHandler(this.timerThread_Tick);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter1.Location = new System.Drawing.Point(0, 431);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(543, 3);
+            this.splitter1.TabIndex = 36;
+            this.splitter1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.buttonGenerate);
+            this.panel3.Controls.Add(this.numericUpDown1);
+            this.panel3.Controls.Add(this.textBoxDelimiter);
+            this.panel3.Controls.Add(this.buttonSaveFile);
+            this.panel3.Controls.Add(this.buttonGenerateRandom);
+            this.panel3.Controls.Add(this.buttonSelectFile);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(537, 73);
+            this.panel3.TabIndex = 6;
+            // 
+            // buttonGenerateRandom
+            // 
+            this.buttonGenerateRandom.Location = new System.Drawing.Point(217, 16);
+            this.buttonGenerateRandom.Name = "buttonGenerateRandom";
+            this.buttonGenerateRandom.Size = new System.Drawing.Size(156, 23);
+            this.buttonGenerateRandom.TabIndex = 30;
+            this.buttonGenerateRandom.Text = "Генерировать случайно";
+            this.toolTip1.SetToolTip(this.buttonGenerateRandom, "Генерировать случайное семя и из него адрес");
+            this.buttonGenerateRandom.UseVisualStyleBackColor = true;
+            this.buttonGenerateRandom.Click += new System.EventHandler(this.buttonGenerateRandom_Click);
+            // 
+            // textBoxDelimiter
+            // 
+            this.textBoxDelimiter.Location = new System.Drawing.Point(135, 19);
+            this.textBoxDelimiter.Name = "textBoxDelimiter";
+            this.textBoxDelimiter.Size = new System.Drawing.Size(76, 20);
+            this.textBoxDelimiter.TabIndex = 35;
+            this.textBoxDelimiter.Tag = "";
+            this.textBoxDelimiter.Text = " |";
+            this.toolTip1.SetToolTip(this.textBoxDelimiter, "разграничитель между ключами");
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1316134911,
+            2328,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Enabled = false;
+            this.buttonGenerate.Location = new System.Drawing.Point(217, 45);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(156, 23);
+            this.buttonGenerate.TabIndex = 6;
+            this.buttonGenerate.Text = "Генерировать из файла";
+            this.toolTip1.SetToolTip(this.buttonGenerate, "чтение семени из файла и генерация для каждого адреса");
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerateFromFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Разделитель:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Количетсво:";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxText.Location = new System.Drawing.Point(3, 82);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxText.Size = new System.Drawing.Size(537, 268);
+            this.textBoxText.TabIndex = 5;
+            this.textBoxText.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBarUpdateBase);
+            this.panel1.Controls.Add(this.buttonStop);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 394);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(537, 34);
+            this.panel1.TabIndex = 0;
+            // 
+            // progressBarUpdateBase
+            // 
+            this.progressBarUpdateBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBarUpdateBase.Location = new System.Drawing.Point(0, 0);
+            this.progressBarUpdateBase.Name = "progressBarUpdateBase";
+            this.progressBarUpdateBase.Size = new System.Drawing.Size(462, 34);
+            this.progressBarUpdateBase.TabIndex = 25;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxText, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.3796F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.6204F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(543, 431);
+            this.tableLayoutPanel1.TabIndex = 37;
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxLog.Location = new System.Drawing.Point(0, 434);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLog.Size = new System.Drawing.Size(543, 94);
+            this.textBoxLog.TabIndex = 4;
+            this.textBoxLog.WordWrap = false;
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(369, 10);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(13, 13);
+            this.labelCount.TabIndex = 27;
+            this.labelCount.Text = "_";
+            this.toolTip1.SetToolTip(this.labelCount, "счетчик");
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.linkLabel1.Location = new System.Drawing.Point(3, 10);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(79, 13);
+            this.linkLabel1.TabIndex = 32;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://upad.ru";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.labelCount);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 356);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(537, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "Очистить список адресов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Dock = System.Windows.Forms.DockStyle.Right;
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Image = global::BitcoinAddressesGenerator.Properties.Resources.delete;
+            this.buttonStop.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStop.Location = new System.Drawing.Point(462, 0);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 34);
+            this.buttonStop.TabIndex = 29;
+            this.buttonStop.Text = "      Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Image = global::BitcoinAddressesGenerator.Properties.Resources.save;
+            this.buttonSaveFile.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSaveFile.Location = new System.Drawing.Point(379, 16);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(148, 23);
+            this.buttonSaveFile.TabIndex = 34;
+            this.buttonSaveFile.Text = "Сохранить результат";
+            this.buttonSaveFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolTip1.SetToolTip(this.buttonSaveFile, "сохранить в файл");
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Image = global::BitcoinAddressesGenerator.Properties.Resources.folder_2_;
+            this.buttonSelectFile.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSelectFile.Location = new System.Drawing.Point(106, 45);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(105, 23);
+            this.buttonSelectFile.TabIndex = 33;
+            this.buttonSelectFile.Text = "Открыть файл";
+            this.buttonSelectFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolTip1.SetToolTip(this.buttonSelectFile, "Открыть файл");
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 528);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.textBoxLog);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Генератор адресов биткоина";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerThread;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBoxDelimiter;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.Button buttonGenerateRandom;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBarUpdateBase;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
